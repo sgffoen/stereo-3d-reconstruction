fcec1f748efe3c6f4bb740c30f9cb301718d8ceb
Simon Griffioen <simon.griffioen@gmail.com>
2021-11-24 10:48:59 +0100

    oakd record and config

--- sensors/oakd_rgbd_record.py
+++ sensors/oakd_rgbd_record.py
@@ -1,16 +1,28 @@
 import cv2
 import numpy as np
 import depthai as dai
+import open3d as o3d
+import tempfile
+import os
+import json
 
+"""
+USER INPUT
+"""
 # write frames
 writeFrames = True
+# filter
+medianFilter = "7x7"
 # Optional. If set (True), the ColorCamera is downscaled from 1080p to 720p.
 # Otherwise (False), the aligned depth is automatically upscaled to 1080p
 downscaleColor = True
-fps = 5
+fps = 10
 # The disparity is computed at this resolution, then upscaled to RGB resolution
 # Select the camera sensor resolution: 1280×720, 1280×800, 640×400 (THE_720_P, THE_800_P, THE_400_P)
 monoResolution = dai.MonoCameraProperties.SensorResolution.THE_400_P
+"""
+END USER INPUT
+"""
 
 # Create pipeline
 pipeline = dai.Pipeline()
@@ -46,7 +58,19 @@ right.setResolution(monoResolution)
 right.setBoardSocket(dai.CameraBoardSocket.RIGHT)
 right.setFps(fps)
 
-stereo.initialConfig.setConfidenceThreshold(245)
+medianMap = {
+    "OFF": dai.StereoDepthProperties.MedianFilter.MEDIAN_OFF,
+    "3x3": dai.StereoDepthProperties.MedianFilter.KERNEL_3x3,
+    "5x5": dai.StereoDepthProperties.MedianFilter.KERNEL_5x5,
+    "7x7": dai.StereoDepthProperties.MedianFilter.KERNEL_7x7,
+}
+if medianFilter not in medianMap:
+    exit("Unsupported median size!")
+
+median = medianMap[medianFilter]
+
+stereo.initialConfig.setConfidenceThreshold(200)
+stereo.initialConfig.setMedianFilter(median)  # KERNEL_7x7 default
 # LR-check is required for depth alignment
 stereo.setLeftRightCheck(True)
 stereo.setDepthAlign(dai.CameraBoardSocket.RGB)
@@ -55,17 +79,24 @@ stereo.setDepthAlign(dai.CameraBoardSocket.RGB)
 camRgb.isp.link(rgbOut.input)
 left.out.link(stereo.left)
 right.out.link(stereo.right)
-stereo.disparity.link(depthOut.input)
+stereo.depth.link(depthOut.input) #stereo.disparity.link(depthOut.input)
 
 # Connect to device and start pipeline
 with dai.Device(pipeline) as device:
+    fd, path = tempfile.mkstemp(suffix='.json')
+    with os.fdopen(fd, 'w') as tmp:
+        json.dump({
+            "width": 1280,
+            "height": 720,
+            "intrinsic_matrix": [item for row in device.get_right_intrinsic() for item in row]
+        }, tmp)
 
     device.getOutputQueue(name="rgb",   maxSize=4, blocking=False)
     device.getOutputQueue(name="depth", maxSize=4, blocking=False)
 
     frameRgb = None
     frameDepth = None
-    count=0
+    frame_cnt=0
     while True:
         latestPacket = {}
         latestPacket["rgb"] = None
@@ -85,7 +116,7 @@ with dai.Device(pipeline) as device:
             frameDepth = latestPacket["depth"].getFrame()
             maxDisparity = stereo.initialConfig.getMaxDisparity()
             # Optional, extend range 0..95 -> 0..255, for a better visualisation
-            if 1: frameDepth = (frameDepth * 255. / maxDisparity).astype(np.uint8)
+            #if 1: frameDepth = (frameDepth * 255. / maxDisparity).astype(np.uint8)
             # Optional, apply false colorization
             #if 1: frameDepth = cv2.applyColorMap(frameDepth, cv2.COLORMAP_HOT)
             frameDepth = np.ascontiguousarray(frameDepth)
@@ -94,17 +125,23 @@ with dai.Device(pipeline) as device:
 
         # Blend when both received
         if frameRgb is not None and frameDepth is not None:
-            # Need to have both frames in BGR format before blending
-            if len(frameDepth.shape) < 3:
-                frameDepth = cv2.cvtColor(frameDepth, cv2.COLOR_GRAY2BGR)
-            # TODO add a slider to adjust blending ratio
-            blended = cv2.addWeighted(frameRgb, 0.6, frameDepth, 0.4 ,0)
-            cv2.imshow("rgb-depth", blended)
-            skip = 3*fps # skip first 3 seconds before writing frames:
-            if writeFrames is True and count>skip:
-                cv2.imwrite("data/depth/framedepth%d.png" % (count-skip), frameDepth)
-                cv2.imwrite("data/image/framergb%d.png" % (count-skip), frameRgb)
-            count+=1
+            # # Need to have both frames in BGR format before blending
+            # if len(frameDepth.shape) < 3:
+            #     frameDepth = cv2.cvtColor(frameDepth, cv2.COLOR_GRAY2BGR)
+            # # TODO add a slider to adjust blending ratio
+            # blended = cv2.addWeighted(frameRgb, 0.6, frameDepth, 0.4 ,0)
+            # cv2.imshow("rgb-depth", blended)
+            rgb = o3d.geometry.Image((frameRgb).astype(np.uint8))
+            depth = o3d.geometry.Image((frameDepth).astype(np.uint16))
+            rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(rgb, depth)
+
+            skip = 5*fps # skip first 5 seconds before writing frames
+            if writeFrames is True and frame_cnt>skip:
+                o3d.io.write_image("../dataset/depth/depth{}.png".format(str(frame_cnt-skip).zfill(5)), depth)
+                o3d.io.write_image("../dataset/image/rgb{}.png".format(str(frame_cnt-skip).zfill(5)), rgb)
+                #cv2.imwrite("data/depth/framedepth%d.png" % (frame_cnt-skip), frameDepth)
+                #cv2.imwrite("data/image/framergb%d.png" % (frame_cnt-skip), frameRgb)
+            frame_cnt+=1
             frameRgb = None
             frameDepth = None
 
